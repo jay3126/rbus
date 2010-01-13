c3d9ae563645b53b59d4e692b2fd962f214a6d65
Siddharth <svs@intellecap.net>
2010-01-14 02:39:14 +0530

    some small features

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -2,7 +2,7 @@ module Merb
   module GlobalHelpers
     # helpers defined here available to all views.  
     def show_rocket?
-      request.route.to_s == "/" and ((session.user and session.user.trips.blank?) or (not session.user))
+      request.route.to_s == "/" and  (not session.user)
     end
 
     def twitter_widget
