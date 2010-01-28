fcc7fa24a4bb4a854c380f08af94c4938cc6c27e
Siddharth <svs@intellecap.net>
2010-01-28 18:44:02 +0530

    trip mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,6 +5,10 @@ class ContactMailer < Merb::MailController
     render_mail
   end
 
+  def new_trip
+    render_mail
+  end
+
   def notify_on_event
     # use params[] passed to this controller to get data
     # read more at http://wiki.merbivore.com/pages/mailers
